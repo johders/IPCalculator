0a56b96114d6b60be5dc50c3146b3edba3f77a72
jd-HW <johannes.dereuddre@student.howest.be>
2024-02-23 09:07:34 +0100

    Update class to load all combobox values

---
+++ IPCalculator.Core/Service/ComboBoxDataLoader.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPCalculator.Core.Service
+{
+    public static class ComboBoxDataLoader
+    {
+        public static IEnumerable<int> AddFirstNumberOptions()
+        {
+            List <int>firstNumberOptions = new List<int>();
+
+            firstNumberOptions.Add(10);
+            firstNumberOptions.Add(172);
+            firstNumberOptions.Add(192);
+
+            return firstNumberOptions.AsReadOnly();
+        }
+
+        public static IEnumerable<int> LoadSecondNumberOptions(int firstNumberSelection)
+        {
+            List<int> secondNumberOptions = new List<int>();
+
+            if (firstNumberSelection == 10)
+            {
+                for (int i = 0; i <= 255; i++)
+                {
+                    secondNumberOptions.Add(i);
+                }
+            }
+
+            if (firstNumberSelection == 172)
+            {
+                for (int i = 16; i <= 31; i++)
+                {
+                    secondNumberOptions.Add(i);
+                }
+            }
+
+            if (firstNumberSelection == 192)
+            {
+                secondNumberOptions.Add(168);
+            }
+
+            return secondNumberOptions.AsReadOnly();
+        }
+
+        public static IEnumerable<int> LoadThirdNumberOption()
+        {
+            List<int> thirdNumberOptions = new List<int>();
+
+
+            for (int i = 0; i <= 255; i++)
+            {
+                thirdNumberOptions.Add(i);
+            }
+
+            return thirdNumberOptions.AsReadOnly();
+
+        }
+
+        public static IEnumerable<int> LoadFourthNumberOption()
+        {
+            List<int> fourthNumberOptions = new List<int>();
+
+            for (int i = 1; i <= 244; i++)
+            {
+                fourthNumberOptions.Add(i);
+            }
+
+            return fourthNumberOptions.AsReadOnly();
+        }
+
+        public static IEnumerable<int> LoadCidrValues(int firstNumberSelection)
+        {
+            List<int> cidrValueOptions = new List<int>();
+
+            if (firstNumberSelection == 10)
+            {
+                for (int i = 8; i <= 30; i++)
+                {
+                    cidrValueOptions.Add(i);
+                }
+            }
+
+            if (firstNumberSelection == 172)
+            {
+                for (int i = 12; i <= 30; i++)
+                {
+                    cidrValueOptions.Add(i);
+                }
+            }
+
+            if (firstNumberSelection == 192)
+            {
+                for (int i = 16; i <= 30; i++)
+                {
+                    cidrValueOptions.Add(i);
+                }
+            }
+
+            return cidrValueOptions.AsReadOnly();
+        }
+    }
+}
--- IPCalculator.Wpf/MainWindow.xaml.cs
+++ IPCalculator.Wpf/MainWindow.xaml.cs
@@ -41,29 +41,45 @@ namespace IPCalculator.Wpf
             DirectoryInfo directoryInfo = new DirectoryInfo(pad);
             directoryInfo = new DirectoryInfo(directoryInfo.Parent.Parent.Parent.FullName);
             Uri uri;
-            uri = new Uri(directoryInfo.FullName + "/Images/green.png");
+            uri = new Uri(directoryInfo.FullName + "/Images/white.png");
             imgColor.Source = new BitmapImage(uri);
         }
         private void CmbHost1B1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if(cmbHost1B1.SelectedItem != null)
             {
+                cmbHost1B2.ItemsSource = null;
+                cmbHost1B3.ItemsSource = null;
+                cmbHost1B4.ItemsSource = null;
+                cmbHost1CIDR.ItemsSource = null;
                 int selectionFirstNumber = (int)cmbHost1B1.SelectedItem;
-                cmbHost1B2.ItemsSource = ipCalculatorService.LoadSecondNumberOptions(selectionFirstNumber);
+                cmbHost1B2.ItemsSource = ComboBoxDataLoader.LoadSecondNumberOptions(selectionFirstNumber);
             }         
         }
 
         private void CmbHost1B2_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-           
+           if(cmbHost1B2.SelectedItem != null)
+            {
+                cmbHost1B3.ItemsSource = ComboBoxDataLoader.LoadThirdNumberOption();
+            }
         }
 
         private void CmbHost1B3_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (cmbHost1B3.SelectedItem != null)
+            {
+                cmbHost1B4.ItemsSource = ComboBoxDataLoader.LoadFourthNumberOption();
+            }
         }
 
         private void CmbHost1B4_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (cmbHost1B4.SelectedItem != null)
+            {
+                int selectionFirstNumber = (int)cmbHost1B1.SelectedItem;
+                cmbHost1CIDR.ItemsSource = ComboBoxDataLoader.LoadCidrValues(selectionFirstNumber);
+            }
         }
 
         private void CmbHost1CIDR_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -74,31 +90,50 @@ namespace IPCalculator.Wpf
         {
             if (cmbHost2B1.SelectedItem != null)
             {
+                cmbHost2B2.ItemsSource = null;
+                cmbHost2B3.ItemsSource = null;
+                cmbHost2B4.ItemsSource = null;
+                cmbHost2CIDR.ItemsSource = null;
                 int selectionFirstNumber = (int)cmbHost2B1.SelectedItem;
-                cmbHost2B2.ItemsSource = ipCalculatorService.LoadSecondNumberOptions(selectionFirstNumber);
+                cmbHost2B2.ItemsSource = ComboBoxDataLoader.LoadSecondNumberOptions(selectionFirstNumber);
             }
         }
 
         private void CmbHost2B2_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (cmbHost2B2.SelectedItem != null)
+            {
+                cmbHost2B3.ItemsSource = ComboBoxDataLoader.LoadThirdNumberOption();
+            }
         }
 
         private void CmbHost2B3_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (cmbHost2B3.SelectedItem != null)
+            {
+                cmbHost2B4.ItemsSource = ComboBoxDataLoader.LoadFourthNumberOption();
+            }
+        }
+
+        private void CmbHost2B4_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbHost2B4.SelectedItem != null)
+            {
+                int selectionFirstNumber = (int)cmbHost2B1.SelectedItem;
+                cmbHost2CIDR.ItemsSource = ComboBoxDataLoader.LoadCidrValues(selectionFirstNumber);
+            }
         }
 
         private void CmbHost2CIDR_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
         }
 
-        private void CmbHost2B4_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-        }
+
 
         void PopulateFirstComboBoxes()
-        {
-            cmbHost1B1.ItemsSource = ipCalculatorService.AddFirstNumberOptions();
-            cmbHost2B1.ItemsSource = ipCalculatorService.AddFirstNumberOptions();           
+        {       
+            cmbHost1B1.ItemsSource = ComboBoxDataLoader.AddFirstNumberOptions();
+            cmbHost2B1.ItemsSource = ComboBoxDataLoader.AddFirstNumberOptions();
         }
 
        
